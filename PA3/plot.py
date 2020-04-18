87c54f6007c4fb93c9ef7f521bae998925e6c442
Richard Red <richardred.q@gmail.com>
2020-04-18 01:35:34 -0400

    test plot.py

--- PA3/plot.py
+++ PA3/plot.py
@@ -1,7 +1,34 @@
 #!/usr/bin/python
 import sys
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-plt.plot([1, 2, 3, 4])
-plt.ylabel('some numbers')
+temps2, temps12, temps19, times2, times12, times19 = ([] for i in range(6))
+for line in sys.stdin:
+    line = line.strip()
+    #print(line)
+    Time, TargetTemp, ActualTemp, BuildingID, sys_bID = line.split('\t')
+    Time = Time.split(':')
+    BuildingID = int(BuildingID)
+
+    try:
+        hours = int(Time[0])
+    except ValueError:
+        continue
+
+    if hours > 8 and hours < 17:
+        if BuildingID == 2:
+            temps2.append(int(ActualTemp))
+            times2.append(hours)
+        elif BuildingID == 12:
+            temps12.append(int(ActualTemp))
+            times12.append(hours)
+        elif BuildingID == 19:
+            temps19.append(int(ActualTemp))
+            times19.append(hours)          
+
+plt.plot(times2, temps2)
+plt.plot(times12, temps12)
+plt.plot(times19, temps19)        
+
+plt.ylabel('Average Temperatures of Buildings #2, 12, and 19 from 8am to 5pm')
 plt.show()
